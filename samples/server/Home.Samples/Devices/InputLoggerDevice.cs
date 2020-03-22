adb1fd45c75b16e3ae45426c7ea0df7c7e7b40c4
Luciano Martorella <mad_lux_it@yahoo.it>
2020-03-22 17:45:34 +0100

    - Added digital input simulator and tester device

---
+++ samples/server/Home.Samples/Devices/InputLoggerDevice.cs
@@ -0,0 +1,56 @@
+﻿using Lucky.Home.Sinks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Lucky.Home.Sinks.DigitalInputArraySink;
+
+namespace Lucky.Home.Devices
+{
+    [Device("Digital Input Event Logger")]
+    [RequiresArray(typeof(DigitalInputArraySink))]
+    public class InputLoggerDevice : DeviceBase
+    {
+        private readonly List<SubSink> _inputSinks = new List<SubSink>();
+        private static int s_count = 0;
+        private int _instanceId;
+
+        public InputLoggerDevice()
+        {
+            _instanceId = s_count++;
+        }
+
+        protected override void OnSinkChanged(SubSink removed, SubSink added)
+        {
+            base.OnSinkChanged(removed, added);
+
+            if (removed != null)
+            {
+                ((DigitalInputArraySink)removed.Sink).EventReceived -= HandleEvent;
+                _inputSinks.Remove(removed);
+            }
+            else if (added != null)
+            {
+                ((DigitalInputArraySink)added.Sink).EventReceived += HandleEvent;
+                _inputSinks.Add(added);
+            }
+        }
+
+        protected override Task OnTerminate()
+        {
+            foreach (var input in _inputSinks)
+            {
+                (input.Sink as DigitalInputArraySink).EventReceived -= HandleEvent;
+            }
+            return base.OnTerminate();
+        }
+
+        private void HandleEvent(object sender, EventReceivedEventArgs e)
+        {   
+            if (_inputSinks.Any(s => s.SubIndex == e.SubIndex && sender == s.Sink))
+            {
+                Console.WriteLine("[{3}]: Input {0} changed at {1}: {2}", e.SubIndex, e.Timestamp, e.State, _instanceId);
+            }
+        }
+    }
+}
--- samples/server/Home.Samples/Sinks/DigitalInputArraySink.cs
+++ samples/server/Home.Samples/Sinks/DigitalInputArraySink.cs
@@ -1,59 +1,148 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
-#pragma warning disable 649
 
 namespace Lucky.Home.Sinks
 {
     /// <summary>
-    /// Passive/poll based switch array
+    /// Poll-based switch set. Not suitable for real-time event reaction. Best suitable for historical event logging.
+    /// Polls the status of each digital port, and receives the historical event log about last changes, with high-resolution timestamp.
     /// </summary>
     [SinkId("DIAR")]
     internal class DigitalInputArraySink : SinkBase
     {
-        public TimeSpan PollPeriod = TimeSpan.FromSeconds(5);
-        private bool[] _status = new bool[0];
+        /// <summary>
+        /// Poll events every few seconds. No need for further precision since the history is stored in the remote device.
+        /// </summary>
+        private TimeSpan PollPeriod = TimeSpan.FromSeconds(5);
+
+        /// <summary>
+        /// The sink state changed in a moment in time (past). 
+        /// </summary>
+        private class BulkChangeEvent
+        {
+            /// <summary>
+            /// Event timestamp
+            /// </summary>
+            public DateTime Timestamp;
+
+            /// <summary>
+            /// All switches state
+            /// </summary>
+            public bool[] State;
+
+            /// <summary>
+            /// If the event is syntethic, raised since the cached data differs from the current sink data.
+            /// </summary>
+            public bool Recovered;
+        }
+
+        /// <summary>
+        /// A single input (sub-sink) changed in a moment in time (past)
+        /// </summary>
+        public class EventReceivedEventArgs : EventArgs
+        {
+            /// <summary>
+            /// Event timestamp
+            /// </summary>
+            public DateTime Timestamp;
+
+            /// <summary>
+            /// Single switch state (sub-sink)
+            /// </summary>
+            public bool State;
+
+            /// <summary>
+            /// SubSink index
+            /// </summary>
+            public int SubIndex;
+        }
+
+        private bool[] _lastState;
 
         protected async override Task OnInitialize()
         {
             await base.OnInitialize();
-            byte[] lastData = null;
             await Read(async reader =>
             {
                 var resp = await reader.Read<ReadStatusResponse>();
-                SubCount = resp.SwitchesCount;
-                lastData = resp.Data;
-                var ret = new bool[SubCount];
-                for (int i = 0; i < SubCount; i++)
-                {
-                    ret[i] = (lastData[i / 8] & (1 << (i % 8))) != 0;
-                }
-                Status = ret;
+                _lastState = resp.CurrentState.State;
+                SubCount = _lastState.Length;
             });
 
-            RunLoop(lastData);
+            // Don't await this
+            RunLoop();
         }
 
         private class ReadStatusResponse : ISerializable
         {
             /// <summary>
-            /// Number of switches
+            /// Current state and time
             /// </summary>
-            public int SwitchesCount; 
+            public BulkChangeEvent CurrentState;
 
             /// <summary>
-            /// Switch data in bytes 
+            /// Previous observed changed events
             /// </summary>
-            public byte[] Data;
+            public BulkChangeEvent[] Events;
+
+            private ulong FromTicks(byte[] ticks)
+            {
+                switch (ticks.Length)
+                {
+                    case 1:
+                        return ticks[0];
+                    case 2:
+                        return BitConverter.ToUInt16(ticks, 0);
+                    case 4:
+                        return BitConverter.ToUInt32(ticks, 0);
+                    case 8:
+                        return BitConverter.ToUInt64(ticks, 0);
+                    default:
+                        throw new InvalidOperationException("Tick size not supported: " + ticks.Length);
+                }
+            }
+
+            private bool[] FromBits(byte[] data, int bitCount)
+            {
+                var ret = new bool[bitCount];
+                for (int i = 0; i < bitCount; i++)
+                {
+                    ret[i] = (data[i / 8] & (1 << (i % 8))) != 0;
+                }
+                return ret;
+            }
 
             public async Task Deserialize(Func<int, Task<byte[]>> feeder)
             {
+                // Read tick size and bit count
                 byte[] size = await feeder(1);
                 // Size is in bits
-                SwitchesCount = size[0];
-                int len = (SwitchesCount - 1) / 8 + 1;
-                Data = await feeder(len);
+                int bitCount = size[0] >> 4;
+                int tickSize = size[0] & 0xf;
+
+                int byteLen = (bitCount - 1) / 8 + 1;
+                var currentState = await feeder(byteLen);
+
+                // Now read ticks/seconds
+                ulong ticksPerSecond = FromTicks(await feeder(tickSize));
+                // Align hw timestamp with the receiver
+                ulong nowT = FromTicks(await feeder(tickSize));
+                DateTime now = DateTime.Now;
+
+                CurrentState = new BulkChangeEvent { State = FromBits(currentState, bitCount), Timestamp = now };
+
+                int l = (await feeder(1))[0];
+                Events = new BulkChangeEvent[l];
+                for (int i = 0; i < l; i++)
+                {
+                    var time = FromTicks(await feeder(tickSize));
+                    var data = await feeder(byteLen);
+                    ulong delta = nowT - time;
+                    DateTime timestamp = now - TimeSpan.FromSeconds((double)delta / ticksPerSecond);
+                    Events[i] = new BulkChangeEvent { State = FromBits(data, bitCount), Timestamp = timestamp };
+                }
             }
 
             public byte[] Serialize()
@@ -62,51 +151,57 @@ namespace Lucky.Home.Sinks
             }
         }
 
-        public bool[] Status
-        {
-            get { return _status; }
-            private set
-            {
-                _status = value;
-                SubCount = _status.Length;
-                StatusChanged?.Invoke(this, EventArgs.Empty);
-            }
-        }
-
-        private async void RunLoop(byte[] lastData)
+        private async void RunLoop()
         {
             while (true)
             {
                 await Task.Delay(PollPeriod);
 
-                bool[] ret = null;
+                var events = new List<EventReceivedEventArgs>();
                 await Read(async reader =>
                 {
                     var resp = await reader.Read<ReadStatusResponse>();
                     if (resp != null)
                     {
-                        if (!resp.Data.SequenceEqual(lastData))
+                        // It is possible to receive no events but data changed compared to the cached copy... In that case creates a changed event
+                        // with the current time.
+                        if (resp.Events.Length == 0 && !resp.CurrentState.State.SequenceEqual(_lastState))
                         {
-                            // Something changed
-                            int swCount = Math.Min(resp.SwitchesCount, resp.Data.Length * 8);
-                            ret = new bool[swCount];
-                            for (int i = 0; i < swCount; i++)
-                            {
-                                ret[i] = (resp.Data[i / 8] & (1 << (i % 8))) != 0;
-                            }
-                            lastData = resp.Data;
+                            resp.CurrentState.Recovered = true;
+                            resp.Events = new[] { resp.CurrentState };
                         }
+
+                        // Now process events 
+                        foreach (var change in resp.Events)
+                        {
+                            // Calc changed bits and prepare single events
+                            for (int i = Math.Min(_lastState.Length, change.State.Length) - 1; i >= 0; i--)
+                            {
+                                if (_lastState[i] != change.State[i])
+                                {
+                                    events.Add(new EventReceivedEventArgs { State = change.State[i], SubIndex = i, Timestamp = change.Timestamp });
+                                }
+                            }
+                            _lastState = change.State;
+                        }
+
+                        // Sync current state
+                        _lastState = resp.CurrentState.State;
                     }
                 });
 
                 // Outside the Read to avoid reenter of other sinks
-                if (ret != null)
+                var raise = EventReceived;
+                foreach (var evt in events)
                 {
-                    Status = ret;
+                    raise(this, evt);
                 }
             }
         }
 
-        public event EventHandler StatusChanged;
+        /// <summary>
+        /// Event raised when one sub changed (with timestamp)
+        /// </summary>
+        public event EventHandler<EventReceivedEventArgs> EventReceived;
     }
 }
