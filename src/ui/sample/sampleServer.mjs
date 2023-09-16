ad75949f8849e978843885d284e899378728dde4
Luciano Martorella <mad_lux_it@yahoo.it>
2023-09-16 11:23:56 +0200

    - Fixed port

--- src/ui/sample/sampleServer.mjs
+++ src/ui/sample/sampleServer.mjs
@@ -3,6 +3,8 @@ import compression from "compression";
 import bodyParser from "body-parser";
 import * as solar from "../index.mjs";
 
+const port = 8081;
+
 const app = express();
 app.use(express.json());
 app.use(compression());
@@ -12,6 +14,6 @@ app.use(bodyParser.raw({ type: "application/octect-stream" }));
 // Register custom endpoints
 solar.register(app, "../dist/csv");
 
-app.listen(80, () => {
-    console.log("REST server started at port 8081");
+app.listen(port, () => {
+    console.log(`REST server started at http://localhost:${port}`);
 });
