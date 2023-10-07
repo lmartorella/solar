fc8651589ac2a2a97f142389c7dd5be567e74c3f
Luciano Martorella <mad_lux_it@yahoo.it>
2023-10-07 16:00:48 +0200

    - Fixed errorResponse call, rtu id was missing - Fixed recovery of first byte

--- mocks/sofar-reg-scan/stresstest.js
+++ mocks/sofar-reg-scan/stresstest.js
@@ -9,7 +9,8 @@ const showCliErr = () => {
 `Expected hostname and testcode
 
 Test codes:
-    1. Test fast bursts of a registry range
+    1. Infinite loop of fast bursts of a registry range
+    2. Single burst of a registry range read
 `);
     process.exit(1);
 };
@@ -20,6 +21,9 @@ if (process.argv.length < 4) {
 
 // Timeout to abandon the query
 const TIMEOUT = 250;
+const BASE_ADDRESS = 0x484;
+const END_ADDRESS = 0x48e;  // grid data
+const BURST_SIZE = 10;
 
 const host = process.argv[2];
 const testCode = Number(process.argv[3]);
@@ -28,43 +32,53 @@ let calls = 0;
 let successfulCalls = 0;
 
 const asleep = t => new Promise(resolve => setTimeout(() => resolve(), t));
-const test1 = async () => {
-    const BURST_COUNT = 10;
-    const BASE_ADDRESS = 0x484;
-    const END_ADDRESS = 0x48e;  // grid data
-    while (true) {
-        const results = [];
-        try {
-            for (let n = 0; n < BURST_COUNT; n++) {
-                // Without any sleep in the middle
-                try {
-                    calls++;
-                    await client.readHoldingRegisters(BASE_ADDRESS, END_ADDRESS - BASE_ADDRESS + 1);
-                    successfulCalls++;
-                    // Ok
-                    results.push("O");
-                } catch (err) {
-                    if (err.err === "Timeout") {
-                        // Timeout
-                        results.push(".");
-                    } else if (err.err === "ModbusException") {
-                        // Modbus error
-                        results.push("E");
-                    } else {
-                        throw err;
-                    }
+
+const doBurstRead = async () => {
+    const results = [];
+    try {
+        for (let n = 0; n < BURST_SIZE; n++) {
+            // Without any sleep in the middle
+            try {
+                calls++;
+                await client.readHoldingRegisters(BASE_ADDRESS, END_ADDRESS - BASE_ADDRESS + 1);
+                successfulCalls++;
+                // Ok
+                results.push("O");
+            } catch (err) {
+                if (err.err === "Timeout") {
+                    // Timeout
+                    results.push(".");
+                } else if (err.err === "ModbusException") {
+                    // Modbus error
+                    results.push("E");
+                } else {
+                    throw err;
                 }
             }
-        } finally {
-            console.log(results.join(""));
         }
+    } finally {
+        console.log(results.join(""));
+    }
+};
+
+const test1 = async () => {
+    while (true) {
+        await doBurstRead();
         await asleep(TIMEOUT);
     }
 }
 
+const test2 = () => {
+    return doBurstRead();
+}
+
+const showResults = () => {
+    console.log(`Success ratio ${(successfulCalls / calls * 100).toFixed(1)}%`);
+};
+
 process.on('SIGINT', async () => {
     console.log("Caught interrupt signal");
-    console.log(`Success ratio ${(successfulCalls / calls * 100).toFixed(1)}%`);
+    showResults();
     process.exit(0);
 });
 
@@ -74,6 +88,9 @@ const main = async () => {
             case 1:
                 await test1();
                 break;
+            case 2:
+                await test2();
+                break;
             default:
                 showCliErr();
         }
@@ -81,6 +98,8 @@ const main = async () => {
         console.error(`EXC: ${err.err ||  "<Unknown>"} ${err.message}`);
         process.exit(1);
     }
+    showResults();
+    socket.destroy();
 };
 
 const client = new Modbus.client.TCP(socket, 1, TIMEOUT);
