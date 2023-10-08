6c19045eca2b9e3e5e3d79ac10a1ebc7b686fd77
Luciano Martorella <mad_lux_it@yahoo.it>
2023-10-08 21:56:56 +0200

    - Tester for lost messages

--- mocks/tcp-sofar-mock/server.js
+++ mocks/tcp-sofar-mock/server.js
@@ -5,7 +5,8 @@ import { onKeyPress } from "./keypress.js";
 const inverterBuffer = Buffer.alloc(0x2000 * 2);
 const ammeterBuffer = Buffer.alloc(0x2000 * 2);
 
-let netServer, modbusServer;
+let netServer;
+let delayNext = false;
 
 const openSocket = () => {
     if (netServer) {
@@ -13,32 +14,30 @@ const openSocket = () => {
     }
     console.log("Open socket");
     netServer = new net.Server();
-    modbusServer = new modbus.server.TCP(netServer, { holding: Buffer.alloc(0x2000 * 2)});
+    const modbusServer = new modbus.server.TCP(netServer, { holding: null });
     netServer.listen(502);
-    modbusServer.on("preReadHoldingRegisters", request => {
-        console.log(` Request to unit ${request.unitId}`);
-        switch (request.unitId) {
-        case 1:
-            inverterBuffer.copy(modbusServer.holding);
-            break;
-        case 2:
-            ammeterBuffer.copy(modbusServer.holding);
-            break;
-        default:
-            throw new Error("Unimplemented unit ID");
-        }
-    });
-};
 
-const closeSocket = () => {
-    if (!netServer) {
-        return;
-    }
-    console.log("Close socket");
-    netServer.close();
-    netServer = null;
-    modbusServer._socket.destroy();
-    modbusServer = null;
+    modbusServer.on("readHoldingRegisters", (request, cb) => {
+        console.log(` Request to unit ${request.unitId}${delayNext ? " (delayed)" : ""}`);
+
+        let holding;
+        switch (request.unitId) {
+            case 1:
+                holding = inverterBuffer;
+                break;
+            case 2:
+                holding = ammeterBuffer;
+                break;
+            default:
+                throw new Error("Unimplemented unit ID");
+        }
+    
+        const responseBody = modbus.responses.ReadHoldingRegistersResponseBody.fromRequest(request.body, holding);
+        const response = modbus.ModbusTCPResponse.fromRequest(request, responseBody);
+        const payload = response.createPayload();
+        setTimeout(() => cb(payload), delayNext ? 3000 : 0);
+        delayNext = false;
+    });
 };
 
 const addresses = { 
@@ -118,12 +117,14 @@ setInterval(() => {
 console.log("Serving at port 502 (modbus)");
 console.log("Serving Sofar inverter at unit ID 1 and ammeter at unit ID 2");
 
-console.log("Press 'q' to quit");
+console.log("Press 'q' to quit, 'd' to delay the next request");
 
 onKeyPress(key => {
     switch (key) {
         case 'q':
             process.exit(0); break;
+        case 'd':
+            delayNext = true; break;
     }
 });
 
