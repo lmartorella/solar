5673a128e53dc267939cfb508112690df4b9e7a8
Luciano Martorella <mad_lux_it@yahoo.it>
2023-10-09 22:10:13 +0200

    - Fixed swapped i18n strings

--- src/server/Home.Solar/Resources.Designer.cs
+++ src/server/Home.Solar/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Lucky.Home {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -92,7 +92,7 @@ namespace Lucky.Home {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Errore: {0}.
+        ///   Looks up a localized string similar to Error: {0}.
         /// </summary>
         internal static string solar_error_mail_error {
             get {
@@ -101,7 +101,7 @@ namespace Lucky.Home {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to , risolto dopo {0} secondi..
+        ///   Looks up a localized string similar to , solved after {0} seconds..
         /// </summary>
         internal static string solar_error_mail_error_solved {
             get {
