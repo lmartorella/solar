5e10bb476d824bdfa3ea7010df4110e8c7a59e2c
Luciano Martorella <mad_lux_it@yahoo.it>
2023-10-28 11:50:41 +0200

    - Reimport Sofar recovery code from library

--- esp-tcp-modbus-bridge/src/main.cpp
+++ esp-tcp-modbus-bridge/src/main.cpp
@@ -25,6 +25,14 @@ static WiFiUDP udp;
 static MDNS mdns(udp);
 #endif
 
+class SofarModbusBridge : public TelnetModbusBridge { 
+protected:
+    /**
+     * Fix frame errors of the first byte due to bus arbitration/drive switch 
+     */
+    virtual void tryFixFrame(uint8_t rtuNodeId, uint8_t requestFunction, Modbus::frame_arg_t* frameArg, uint8_t*& data, uint8_t& len);
+};
+
 static TelnetModbusBridge bridge;
 
 void setup() {
@@ -41,12 +49,6 @@ void setup() {
     _rtuSerial.begin(9600, SERIAL_8N1, 35, 33); // 35 is input only
 #endif
 
-#if defined(ESP8266)
-    bridge.begin(_rtuSerial, 0, TxEnableHigh);
-#else
-    bridge.begin(_rtuSerial, 32, TxEnableHigh);
-#endif
-
     WiFi.setHostname(WIFI_HOSTNAME);
     WiFi.begin(WIFI_SSID, WIFI_PASSPHRASE);
 
@@ -56,6 +58,12 @@ void setup() {
     // system is mDNS/Bonjour-enabled (such as MacOS X).
     // Always call this before any other method!
     mdns.begin(WiFi.localIP(), WIFI_HOSTNAME);
+#endif
+
+#if defined(ESP8266)
+    bridge.begin(_rtuSerial, 0, TxEnableHigh);
+#else
+    bridge.begin(_rtuSerial, 32, TxEnableHigh);
 #endif
 }
 
@@ -68,4 +76,39 @@ void loop() {
 
     bridge.task();
     yield();
+}
+
+/**
+ * Fix frame errors of the first byte due to bus arbitration/drive switch 
+ */
+void SofarModbusBridge::tryFixFrame(uint8_t rtuNodeId, uint8_t requestFunction, Modbus::frame_arg_t* frameArg, uint8_t*& data, uint8_t& len) {
+  if (len == 3 && data[1] == 0x90) {
+    // Shift 1
+    len--;
+    data++;
+  }
+  if (len == 2 && data[0] == 0x90) {
+    // Fix Sofar error
+    data[0] = 0x83;
+    frameArg->validFrame = true;
+    frameArg->slaveId = rtuNodeId;
+    log.printf("Recovered 0x90 error\n");
+  } else if (data[0] == rtuNodeId && data[1] == requestFunction) {
+    // 1-shift is common, the node Id entered in the frame shifting everything up
+    len--;
+    data++;
+    frameArg->validFrame = true;
+    frameArg->slaveId = rtuNodeId;
+    log.printf("Recovered 1-byte shifted frame\n");
+  } else {
+      log.printf("RTU: Invalid frame: ");
+      uint8_t i;
+      for (i = 0; i < len + 2 && i < 64; i++) {
+        log.printf("<%02x>", data[i]);
+      }
+      if (i >= 64) {
+        log.printf("...");
+      }
+      log.printf("\n");
+  }
 }
