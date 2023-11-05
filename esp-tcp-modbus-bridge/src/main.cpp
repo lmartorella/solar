094185b14e4af08ba347e11a3f1cd7768f9a2e0a
Luciano Martorella <mad_lux_it@yahoo.it>
2023-11-05 12:13:44 +0100

    - Reimported Sofar specific code

--- esp-tcp-modbus-bridge/src/main.cpp
+++ esp-tcp-modbus-bridge/src/main.cpp
@@ -46,6 +46,11 @@ void setup() {
 #else
     bridge.begin(_rtuSerial, 32, TxEnableHigh);
 #endif
+
+    // Sofar doesn't follow the modbus spec, and it is splitting messages with more than 3.5 * space time sometimes
+    // ((1 / 9600) * 11) * 3.5 = 4ms
+    // Use 8ms instead
+    bridge.setInterFrameTime(8000);
 }
 
 void loop() {
